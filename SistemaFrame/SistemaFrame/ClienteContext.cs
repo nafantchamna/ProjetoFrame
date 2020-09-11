ce376e6923672374815d78fa4a62d60a16ce3a1d
nafantchamna <cletche1991@gmail.com>
2020-09-11 02:17:10 -0300

    primeira committttt

---
+++ SistemaFrame/SistemaFrame/ClienteContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFrame
+{
+  public  class ClienteContext: DbContext
+    {
+       public DbSet<Cliente> Clientes { get; set; }
+    }
+}
